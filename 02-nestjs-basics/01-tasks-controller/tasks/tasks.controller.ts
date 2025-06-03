1784da4f256d243a59e468440bd631a0ddd2c6ce
Nikolay1727 <cnm17@yandex.ru>
2025-06-03 17:32:54 +0300

    homework 2 - task 1

--- 02-nestjs-basics/01-tasks-controller/tasks/tasks.controller.ts
+++ 02-nestjs-basics/01-tasks-controller/tasks/tasks.controller.ts
@@ -6,26 +6,38 @@ import {
   Param,
   Patch,
   Post,
+  UsePipes,
+  ValidationPipe,
 } from "@nestjs/common";
 import { TasksService } from "./tasks.service";
-import { Task } from "./task.model";
+import { CreateTaskDto, UpdateTaskDto } from "./task.model";
 
 @Controller("tasks")
 export class TasksController {
   constructor(private readonly tasksService: TasksService) {}
 
   @Get()
-  getAllTasks() {}
+  getAllTasks() {
+    return this.tasksService.getAllTasks();
+  }
 
   @Get(":id")
-  getTaskById(@Param("id") id: string) {}
+  getTaskById(@Param("id") id: string) {
+    return this.tasksService.getTaskById(id);
+  }
 
   @Post()
-  createTask(@Body() task: Task) {}
+  createTask(@Body() task: CreateTaskDto) {
+    return this.tasksService.createTask(task);
+  }
 
   @Patch(":id")
-  updateTask(@Param("id") id: string, @Body() task: Task) {}
+  updateTask(@Param("id") id: string, @Body() task: UpdateTaskDto) {
+    return this.tasksService.updateTask(id, task);
+  }
 
   @Delete(":id")
-  deleteTask(@Param("id") id: string) {}
+  deleteTask(@Param("id") id: string) {
+    return this.tasksService.deleteTask(id);
+  }
 }
